3d1b286a1b0e6b9899e50031a913f0195ba1fe00
Haksiu <38468996+Haksiu@users.noreply.github.com>
2022-09-13 20:51:25 +0200

    styles update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import Head from 'next/head'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import useRandomJoke from "../utils/useRandomJoke"
 
 export default function Home() {
-
   const firstNameRef = React.createRef(null);
   const lastNameRef = React.createRef(null);
   const [firstName, setFirstName] = useState("Johnny");
@@ -11,24 +9,23 @@ export default function Home() {
 
   const joke = useRandomJoke(firstName, lastName);
 
-
   const generateRandomJoke = () => {
-   
     setFirstName(firstNameRef.current.value);
     setLastName(lastNameRef.current.value);
   }
-
   return (
-    <div className='p-[16px] text-white'>
+    <div className='md:px-[16px] py-[16px] px-[40px]'>
       <center>
-        <h1 className='text-[30px] mb-4 font-bold'>Random Joke Generator</h1>
-        <p className='mb-10 text-[20px]'>{joke}</p>  
-
-        <form className='bg-gray-400 shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-[400px]'>
-          <input type="text" className='flex justify-center items-center shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'  placeholder='first name' ref={firstNameRef}/>
-          <input type="text" className='flex justify-center items-center shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='last name' ref={lastNameRef}/>
-          <button className='w-full h-[50px] mt-5 bg-gray-500 rounded font-bold' onClick={(e) => generateRandomJoke(e.preventDefault())}>Generate Joke</button>
+        <div className='max-w-[450px] min-h-[150px] bg-slate-300 rounded p-4 shadow-md'>
+        <h1 className='md:text-[35px] mb-4 font-bold text-gray-600 text-[30px]'>Random Joke Generator</h1>
+        <p className='mb-10 text-[20px] text-white text-opacity-[0.7] font-[700]'>{ joke }</p>  
+        
+        <form className='bg-gray-400 shadow-lg rounded px-8 pt-6 pb-8 mb-4 max-w-[400px]'>
+          <input type="text" className='mb-4 text-center flex justify-center items-center shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'  placeholder='First Name' ref={firstNameRef}/>
+          <input type="text" className='flex text-center justify-center items-center shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Last Name' ref={lastNameRef}/>
+          <button className='text-white w-full h-[50px] mt-5 transition duration-300 ease-in-out bg-gray-500 hover:bg-gray-900 hover:opacity-[.6] rounded font-bold' onClick={(e) => generateRandomJoke(e.preventDefault())}>Generate Joke</button>
         </form>
+        </div>
       </center>
     </div>
   )
